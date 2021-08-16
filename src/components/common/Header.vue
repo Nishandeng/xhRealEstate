9f33cfbaec7024617e659c593d86577ddcebd087
nishandeng <31160514+Nishandeng@users.noreply.github.com>
2021-08-16 20:31:30 +0800

    init

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -6,7 +6,7 @@
                 <i v-if="!collapse" class="el-icon-s-fold"></i>
                 <i v-else class="el-icon-s-unfold"></i>
             </div>
-            <div class="logo">益苗荟后台管理系统</div>
+            <div class="logo">旭辉地产后台管理系统</div>
             <div class="header-right">
                 <div class="header-user-con">
                     <!-- 全屏显示 -->
